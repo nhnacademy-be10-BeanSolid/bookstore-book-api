c048e82d22876621b6d78a555be5aff96b5a2751
NaIlHwan <nlhjobs01@naver.com>
2025-06-10 14:42:14 +0900

    refactor: ERD와 동일하게 JPA 컬럼 속성 수정

--- src/main/java/com/nhnacademy/bookapi/bookcategory/domain/BookCategory.java
+++ src/main/java/com/nhnacademy/bookapi/bookcategory/domain/BookCategory.java
@@ -24,12 +24,13 @@ public class BookCategory {
     @OneToMany(mappedBy = "parentCategory", cascade = CascadeType.REMOVE)
     private List<BookCategory> children = new ArrayList<>();
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "DATETIME")
     private LocalDateTime createdAt;
 
+    @Column(columnDefinition = "DATETIME")
     private LocalDateTime updatedAt;
 
     public BookCategory(String name, BookCategory parentCategory) {
