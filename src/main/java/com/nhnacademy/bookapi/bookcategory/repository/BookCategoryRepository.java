b7bf74ee2ee9c33310b31d14bf6a84e9ecb605c8
chosun-nhn16 <tmdals3622@naver.com>
2025-07-14 15:13:20 +0900

    feat: 카테고리 트리 전달 구현

--- src/main/java/com/nhnacademy/bookapi/bookcategory/repository/BookCategoryRepository.java
+++ src/main/java/com/nhnacademy/bookapi/bookcategory/repository/BookCategoryRepository.java
@@ -1,9 +1,11 @@
 package com.nhnacademy.bookapi.bookcategory.repository;
 
 import com.nhnacademy.bookapi.bookcategory.domain.BookCategory;
+import com.nhnacademy.bookapi.bookcategory.domain.response.BookCategoryNodeResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
