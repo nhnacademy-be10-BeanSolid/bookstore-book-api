b37a4a36466d22fdf79b648955c6f20301bec828
chosun-nhn16 <tmdals3622@naver.com>
2025-07-14 10:42:05 +0900

    refactor: 분석기 수정

--- src/main/java/com/nhnacademy/bookapi/document/BookDocument.java
+++ src/main/java/com/nhnacademy/bookapi/document/BookDocument.java
@@ -1,65 +1,75 @@
-    package com.nhnacademy.bookapi.document;
+package com.nhnacademy.bookapi.document;
 
-    import com.nhnacademy.bookapi.book.domain.Book;
-    import com.nhnacademy.bookapi.booktag.domain.BookTag;
-    import jakarta.persistence.Id;
-    import lombok.Getter;
-    import lombok.AllArgsConstructor;
-    import lombok.ToString;
-    import org.springframework.data.elasticsearch.annotations.*;
+import com.nhnacademy.bookapi.book.domain.Book;
+import com.nhnacademy.bookapi.booktag.domain.BookTag;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.*;
 
-    import java.util.Set;
-    import java.util.stream.Collectors;
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-    @ToString
-    @Getter
-    @Document(indexName = "beansolid", writeTypeHint = WriteTypeHint.FALSE)
-    @AllArgsConstructor
-    @Setting(settingPath = "/elasticsearch/settings.json")
-    public class BookDocument {
 
-        @Id
-        @Field(type = FieldType.Keyword)
-        private String id;
+@Getter
+@Document(indexName = "beansolid")
+@AllArgsConstructor
+@Setting(settingPath = "/elasticsearch/settings.json")
+public class BookDocument {
 
-        @MultiField(
-                mainField = @Field(type = FieldType.Text, analyzer = "korean_icu_analyzer"),
-                otherFields = {
-                        @InnerField(suffix = "jaso", type = FieldType.Text, analyzer = "jaso_analyzer"),        // 자소
-                        @InnerField(suffix = "synonym", type = FieldType.Text, analyzer = "synonym_analyzer") // 동의어 분석기
-                }
-        )
-        private String title;
+    @Id
+    @Field(type = FieldType.Keyword)
+    private String id;
 
-        @Field(type = FieldType.Text, analyzer = "korean_icu_analyzer")
-        private String description;
+    @MultiField(
+            mainField = @Field(type = FieldType.Text, analyzer = "korean_analyzer"),
+            otherFields = {
+                    @InnerField(suffix = "jaso", type = FieldType.Text, analyzer = "jaso_analyzer"),        // 자소
+                    @InnerField(suffix = "synonym", type = FieldType.Text, analyzer = "korean_synonym_analyzer"), // 동의어 분석기
+            }
+    )
+    private String title;
 
-        @Field(type = FieldType.Keyword)
-        private String author;
+    //        @Field(type = FieldType.Text, analyzer = "korean_analyzer", termVector = TermVector.with_positions_offsets)
+    @Field(type = FieldType.Text, analyzer = "korean_analyzer")
+    private String description;
 
-        @Field(type = FieldType.Keyword)
-        private String publisher;
+    @Field(type = FieldType.Text, analyzer = "korean_analyzer")
+    private String author;
 
-        @Field(type = FieldType.Keyword)
-        private String isbn;
+    @Field(type = FieldType.Keyword)
+    private String publisher;
 
-        @Field(type = FieldType.Keyword)
-        private Set<String> tags;
+    @Field(type = FieldType.Keyword)
+    private Set<String> tags;
 
-        public static BookDocument from(Book book) {
-            Set<String> tags = book.getBookTags()
-                    .stream()
-                    .map(BookTag::getName)
-                    .collect(Collectors.toSet());
+    @Field(type = FieldType.Date, format = DateFormat.date)
+    private LocalDate publishedAt;
 
-            return new BookDocument(
-                    String.valueOf(book.getId()),
-                    book.getTitle(),
-                    book.getDescription(),
-                    book.getAuthor(),
-                    book.getPublisher(),
-                    book.getIsbn(),
-                    tags
-            );
-        }
+    @Field(type = FieldType.Integer)
+    private Integer salePrice;
+
+    @Field(type = FieldType.Long)
+    private Long viewCount;
+
+    public static BookDocument from(Book book) {
+        Set<String> tags = book.getBookTags()
+                .stream()
+                .map(BookTag::getName)
+                .collect(Collectors.toSet());
+
+        return new BookDocument(
+                String.valueOf(book.getId()),
+                book.getTitle(),
+                book.getDescription(),
+                book.getAuthor(),
+                book.getPublisher(),
+                tags,
+                book.getPublishAt(),
+                book.getSalePrice(),
+                book.getViewCount()
+        );
     }
+}
+
--- src/main/java/com/nhnacademy/bookapi/document/repository/impl/CustomBookDocumentRepositoryImpl.java
+++ src/main/java/com/nhnacademy/bookapi/document/repository/impl/CustomBookDocumentRepositoryImpl.java
@@ -1,19 +1,26 @@
 package com.nhnacademy.bookapi.document.repository.impl;
 
+import co.elastic.clients.elasticsearch._types.SortOptions;
 import co.elastic.clients.elasticsearch._types.SortOrder;
+import com.nhnacademy.bookapi.book.domain.response.SimpleBookResponse;
+import com.nhnacademy.bookapi.book.exception.BookNotFoundException;
+import com.nhnacademy.bookapi.book.repository.BookRepository;
 import com.nhnacademy.bookapi.document.BookDocument;
+import com.nhnacademy.bookapi.book.domain.Book;
 import com.nhnacademy.bookapi.document.repository.CustomBookDocumentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -22,11 +29,27 @@ import java.util.List;
 public class CustomBookDocumentRepositoryImpl implements CustomBookDocumentRepository {
 
     private final ElasticsearchOperations elasticsearchOperations;
+    private final BookRepository bookRepository;
 
     @Override
-    public Page<BookDocument> searchByKeyword(String keyword, Pageable pageable) {
+    public Page<SimpleBookResponse> searchByKeyword(String keyword, Pageable pageable) {
 
         log.info("검색시작");
+
+        // 정렬 정보
+        Sort sort = pageable.getSort();
+        List<SortOptions> sortOptionsList = new ArrayList<>();
+
+        if (sort.isUnsorted()) {
+            sortOptionsList.add(SortOptions.of(s -> s.field(f -> f.field("id").order(SortOrder.Desc))));
+        } else {
+            for (Sort.Order order : sort) {
+                String property = order.getProperty();
+                SortOrder sortOrder = order.isAscending() ? SortOrder.Asc : SortOrder.Desc;
+
+                sortOptionsList.add(SortOptions.of(s -> s.field(f -> f.field(property).order(sortOrder))));
+            }
+        }
 
         // 쿼리 객체
         NativeQuery query = NativeQuery.builder()
@@ -36,26 +59,46 @@ public class CustomBookDocumentRepositoryImpl implements CustomBookDocumentRepos
                                 "title^5",
                                 "title.synonym^5",
                                 "title.jaso^5",
-                                "description^4",
-                                "author",
-                                "publisher",
+                                "description^1",
+                                "author^3",
+                                "publisher^3",
                                 "isbn^5",
                                 "tags^5"
                         )
                 ))
-                .withSort(s -> s.field(f -> f.field("id").order(SortOrder.Desc)))
-                .withPageable(pageable)
+//                .withSort(sortOptionsList)
+                .withPageable(pageable) // 현재 페이지만
                 .build();
-
-        log.info("query {}", query);
 
         // 검색 결과를 담고있는 컨테이너
         SearchHits<BookDocument> hits = elasticsearchOperations.search(query, BookDocument.class); // 검색 실행(쿼리를 보냄)
 
         log.info("hits: {}", hits.getSearchHits());
 
-        List<BookDocument> content = hits.getSearchHits().stream()
-                .map(SearchHit::getContent)
+        // 현재 페이지의 검색 결과 아이디
+        List<String> ids = hits.getSearchHits().stream()
+                .map(SearchHit::getId)
+                .toList();
+
+        // 도서 엔티티를 담을 리스트
+        List<Book> books = new ArrayList<>();
+        for (String id : ids) {
+            Long bookId = Long.valueOf(id);
+            Book book = bookRepository.findById(bookId)
+                    .orElseThrow(() -> new BookNotFoundException(bookId));
+            books.add(book);
+        }
+
+        List<SimpleBookResponse> content = books.stream()
+                .map(book -> new SimpleBookResponse(
+                        book.getId(),
+                        book.getTitle(),
+                        book.getAuthor(),
+                        book.getSalePrice(),
+                        book.getStock(),
+                        book.getImage(),
+                        book.getViewCount()
+                ))
                 .toList();
 
         long total = hits.getTotalHits();
