b1d7ad4404ddb289e01dbc44d777f6162c8edfa8
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-23 08:55:38 +0900

    feat: 관리자 -> 카테고리 쿠폰 발급 구현

--- src/main/java/com/nhnacademy/bookapi/bookcategory/service/impl/BookCategoryServiceImpl.java
+++ src/main/java/com/nhnacademy/bookapi/bookcategory/service/impl/BookCategoryServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,10 +59,11 @@ public class BookCategoryServiceImpl implements BookCategoryService {
     public Page<BookCategoryResponse> getAllCategories(Pageable pageable) {
         return bookCategoryRepository.findAllBookCategoryResponse(pageable);
     }
-
+    //카테고리 정보 목록을 가져와 반환
     @Override
+    @Transactional(readOnly = true)
     public List<BookCategoryResponse> getAllCategories() {
-        return List.of();
+        return bookCategoryRepository.findAllBookCategoryResponse(PageRequest.of(0, Integer.MAX_VALUE)).getContent();
     }
 
     @Override
